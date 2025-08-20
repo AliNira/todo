7513c8710251ae984092b2cb54fc91d362edad1f
Nira@Hami <dev.erfaniii@gmail.com>
2025-08-20 16:41:32 +0330

    Complete Swagger

--- src/main/java/org/nira/todo/TodoApplication.java
+++ src/main/java/org/nira/todo/TodoApplication.java
@@ -1,8 +1,22 @@
 package org.nira.todo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Todo API Documentation",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Ali Nira",
+                        email = "dev.erfaniii@gmail.com",
+                        url = "https://www.linkedin.com/in/ali-nira-49425a21b/"
+                )
+        )
+)
 @SpringBootApplication
 public class TodoApplication {
 
